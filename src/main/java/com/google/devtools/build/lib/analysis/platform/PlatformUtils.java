c4eb49abd27884320b468a02eb51b4067e21880b
Arvind Shelke <arvind.shelke@devdynamics.ai>
2025-08-01 00:32:50 +0530

    Update PlatformUtils.java

--- src/main/java/com/google/devtools/build/lib/analysis/platform/PlatformUtils.java
+++ src/main/java/com/google/devtools/build/lib/analysis/platform/PlatformUtils.java
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 package com.google.devtools.build.lib.analysis.platform;
 
 import build.bazel.remote.execution.v2.Platform;
